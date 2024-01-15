263d9d3971675b9b219347930ddc8b18a84efdd3
sheerin-farhana <sheerinfarhana25@gmail.com>
2024-01-15 20:30:31 +0530

    "simple todo apis done with typescript"

--- typescript-project/dist/routes/todos.js
+++ typescript-project/dist/routes/todos.js
@@ -7,18 +7,21 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text,
     };
     res.status(201).json({ message: "Todo added", todos: todos });
     todos.push(newTodo);
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: 'updated todo', todos: todos });
     }
     res.status(404).json({ message: 'Could not find todo' });
